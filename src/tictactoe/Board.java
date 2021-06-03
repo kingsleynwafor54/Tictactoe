aaf3208ddb9c43580daf4067de8087da36b10d5f
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-06-03 11:33:37 +0100

    initial commit

---
+++ src/tictactoe/Board.java
@@ -0,0 +1,27 @@
+package tictactoe;
+
+public class Board {
+   private Value[][] array = new Value[3][3];
+
+    public Board() {
+//Initialize the array to empty anytime it is called upon
+        for (int row = 0; row < array.length; row ++ ){
+            for (int column = 0; column < array[row].length; column++){
+                array[row][column] = Value.EMPTY;
+            }
+        }
+
+    }
+
+    public Value[][] getArray() {
+        return array;
+    }
+    public void displayBoard(){
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                System.out.print(array[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
